7b4fdafcefddec44be9ee7cd789220f42bbd3dc6
dev_chow <56135351+devchow@users.noreply.github.com>
2021-10-16 22:27:40 +0300

    commit x
    commit x

---
+++ Assets/Scripts/PieceSpawner.cs
@@ -0,0 +1,26 @@
+﻿/*
+ *
+ * Developer => Kelvin Mwangi
+ * 
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceSpawner : MonoBehaviour
+{
+    public PieceType type;
+    private Piece currentPiece;
+    
+    public void Spawn()
+    {
+        //currentPiece = // get me a new Piece from the pool
+        currentPiece.gameObject.SetActive(true);
+        currentPiece.transform.SetParent(transform, false);
+    }
+
+    public void DeSpawn()
+    {
+        currentPiece.gameObject.SetActive(false);
+    }
+}
